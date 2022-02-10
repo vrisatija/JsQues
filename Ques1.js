8978f207e1fee7cab5f3b59f6b9265b615d7b380
vri <Vriti_Satija@mckinsey.com>
2022-02-11 01:04:45 +0530

    feat: add ArmstrongNos

--- Ques1.js
+++ Ques1.js
@@ -1,21 +1,21 @@
-function isArmstrong(n)
+function armstrongNos(n)
 {
-    for(let i=0;i<=n;i++)
+    for(let num=0;num<=n;num++)
     {
         let ans=0
-        let num=i
-        while(num!=0)
+        let currentNum=num
+        while(currentNum!=0)
         {
-            let digit=num%10
+            let digit=currentNum%10
             ans+=parseInt(Math.pow(digit,3))
-            num=parseInt(num/10)
+            currentNum=parseInt(currentNum/10)
         }
     
-        if(ans==i)
+        if(ans===num)
         {
-            console.log(i+ "  ")
+            console.log(num)
         }
     }
 }
 var input=407;
-isArmstrong(input)
+armstrongNos(input)
